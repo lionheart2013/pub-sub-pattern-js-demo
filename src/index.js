5c18c68b2f59a0aaffb35748fdea1cbbd11732cd
Jhunnel <gunblade2318@gmail.com>
2023-02-04 02:10:07 +0800

    finalized implementation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,53 @@
 const EventBusFactory = () => {
     const listeners = {};
 
+    /**
+     * Broadcast payload to subscribed listeners
+     *
+     * @param event
+     * @param payload
+     * @returns {*[]}
+     */
     const fire = (event, payload) => {
-        // publish message here
+        if (listeners.hasOwnProperty(event) && Array.isArray(listeners[event])) {
+            return listeners[event].map(fn => fn(payload));
+        }
+        return [];
     };
 
+    /**
+     * Register an event with listener.
+     *
+     * @param event
+     * @param listener
+     */
     const listen = (event, listener) => {
-        // subscribe a message
+        if (!Array.isArray(listeners[event])) {
+            listeners[event] = [listener];
+            return;
+        }
+        listeners[event].push(listener);
     };
 
+    /**
+     * Unsubscribe based on event and listener
+     *
+     * @param event
+     * @param removeListener
+     * @returns {*[]}
+     */
     const unsubscribe = (event, removeListener) => {
-        // unsubscribe from event
+        if (listeners.hasOwnProperty(event) && Array.isArray(listeners[event])) {
+            listeners[event] = listeners[event].filter(fn => (fn.toString() !== removeListener.toString()));
+        }
+        return listeners[event];
     };
 
     return {
         fire,
         listen,
-        unsubscribe
-    }
-}
+        unsubscribe,
+    };
+};
 
-
-module.exports = EventBusFactory
+module.exports = EventBusFactory;
